65b1194af8b5ef96a5138a404e3d5bb62e3aa912
LasseGuldhammer <echon@echon.dk>
2017-03-21 19:57:35 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en-UK">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>    
+    <body>        
+        <header>
+        	<img class="back-button" src="img/back.png" alt="back button"><img class="grundfos-logo" src="img/grundfos_logo.jpg" alt="Grundfos logo"><img class="home-button" src="img/home.png" alt="home button">
+        </header>
+		<h1 class="title">LOGIN</h1>        
+        <main>
+        	<h2 class="input-heading">EMAIL ADDRESS</h2>
+        	<input class="input" type="text" placeholder="Email address">
+        	<h2 class="input-heading">PASSWORD</h2>
+        	<input class="input" type="password" placeholder="Password">
+        	<button class="login">Login</button>
+        </main>
+        
+        
+
+        
+    </body>
+</html>
